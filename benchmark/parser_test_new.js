1a0c91fb749deac16ef27a2d8bdf00f07358c31c
Nikolay Kostyurin <jilizart@gmail.com>
2018-07-06 00:39:55 +0200

    benchmark files code style

--- benchmark/parser_test_new.js
+++ benchmark/parser_test_new.js
@@ -1,19 +1,19 @@
-const parse = require('../packages/bbob-parser/index');
+const parse = require('../packages/bbob-parser/lib/index');
 
-const options = {
-    closableTags: ['ch', 'syllable', 'tab']
-};
-
-const textStub = require("./test/stub");
+const textStub = require('./test/stub');
 
 const count = 0;
 const parsers3 = [];
 
-console.time('newParser');
-for (let i = 0; i <= count; i++) {
-    const parser3 = parse(textStub, options);
+function test() {
+  console.time('newParser');
+  for (let i = 0; i <= count; i++) {
+    const parser3 = parse(textStub);
 
     parsers3.push(parser3);
+  }
+  console.timeEnd('newParser');
 }
-console.timeEnd('newParser');
+
+test();
 // console.log(JSON.stringify(parsers3));
--- benchmark/parser_test_old.js
+++ benchmark/parser_test_old.js
@@ -1,14 +1,14 @@
-const OldParser = require('./OldParser')
+const OldParser = require('./OldParser');
 
-const textStub = require("./test/stub");
+const textStub = require('./test/stub');
 
 const count = 0;
 const oldParsers3 = [];
 console.time('oldParser');
 for (let i = 0; i <= count; i++) {
-    const oldParser3 = OldParser.parse(textStub);
+  const oldParser3 = OldParser.parse(textStub);
 
-    oldParsers3.push(oldParser3);
+  oldParsers3.push(oldParser3);
 }
 console.timeEnd('oldParser');
 // console.log(JSON.stringify(oldParsers3));
