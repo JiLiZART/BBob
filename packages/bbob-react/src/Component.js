615679905c109a2c443f2d24212ea93acd3f8bf5
Gordeev Nikita <nikita.kolibri@gmail.com>
2019-03-27 14:58:46 +0200

    refactor(react): code style

--- packages/bbob-react/src/Component.js
+++ packages/bbob-react/src/Component.js
@@ -5,8 +5,8 @@ import { render } from './render';
 const content = (children, plugins, options) => React.Children.map(children, child =>
   (typeof child === 'string' ? render(child, plugins, options) : child));
 
-const Component = props =>
-  React.createElement(props.container, { ...props.componentProps }, content(props.children, props.plugins, props.options));
+const Component = ({ container, componentProps, children, plugins, options }) =>
+  React.createElement(container, { ...componentProps }, content(children, plugins, options));
 
 if (process.env.NODE_ENV !== 'production') {
   Component.propTypes = {
