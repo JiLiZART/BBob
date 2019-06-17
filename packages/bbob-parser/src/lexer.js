4bcac7b83f74d5e876a2ebb912af72edb3dd3ccd
David Ferguson <davidferguson@users.noreply.github.com>
2019-06-17 17:21:39 +0100

    more styling fixes

--- packages/bbob-parser/src/lexer.js
+++ packages/bbob-parser/src/lexer.js
@@ -53,7 +53,7 @@ function createLexer(buffer, options = {}) {
   const RESERVED_CHARS = [closeTag, openTag, QUOTEMARK, BACKSLASH, SPACE, TAB, EQ, N, EM];
   const NOT_CHAR_TOKENS = [
     ...(options.enableEscapeTags ? [BACKSLASH] : []),
-    openTag, SPACE, TAB, N, BACKSLASH
+    openTag, SPACE, TAB, N, BACKSLASH,
   ];
   const WHITESPACES = [SPACE, TAB];
   const SPECIAL_CHARS = [EQ, SPACE, TAB];
@@ -158,8 +158,8 @@ function createLexer(buffer, options = {}) {
     } else if (isWhiteSpace(currChar)) {
       const str = bufferGrabber.grabWhile(isWhiteSpace);
       emitToken(createToken(TYPE_SPACE, str, row, col));
-    } else if (options.enableEscapeTags && currChar === BACKSLASH &&
-               (nextChar === openTag || nextChar === closeTag)) {
+    } else if (options.enableEscapeTags && currChar === BACKSLASH
+               && (nextChar === openTag || nextChar === closeTag)) {
       bufferGrabber.skip(); // skip the \ without emitting anything
       bufferGrabber.skip(); // skip past the [ or ] as well
       emitToken(createToken(TYPE_WORD, nextChar, row, col));
