b5216260d626cfd2f6c4afc42562c8c0ba1044e6
Nikolay Kostyurin <jilizart@gmail.com>
2020-01-17 00:29:04 +0200

    fix(jest): add jest support for all packages

---
+++ babel.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ],
+  env: {
+    test: {
+      plugins: [
+        [
+          '@babel/plugin-transform-modules-commonjs',
+          {
+            loose: true,
+          },
+        ],
+      ],
+    },
+    commonjs: {
+      plugins: [
+        [
+          '@babel/plugin-transform-modules-commonjs',
+          {
+            loose: true,
+          },
+        ],
+      ],
+    },
+    rollup: {
+      // plugins: [
+      //   '@babel/plugin-external-helpers',
+      // ],
+    },
+  },
+};
