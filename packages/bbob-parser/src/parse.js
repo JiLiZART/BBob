9a668db5c861dceb4dec1d70a2118243f3676d3f
Nikolay Kost <JiLiZART@Gmail.com>
2023-09-28 00:49:40 +0000

    fix: lint

--- packages/bbob-parser/src/parse.js
+++ packages/bbob-parser/src/parse.js
@@ -20,7 +20,7 @@ const parse = (input, opts = {}) => {
   const options = opts;
   const openTag = options.openTag || OPEN_BRAKET;
   const closeTag = options.closeTag || CLOSE_BRAKET;
-  const onlyAllowTags = (options.onlyAllowTags || []).filter(Boolean).map(tag => tag.toLowerCase());
+  const onlyAllowTags = (options.onlyAllowTags || []).filter(Boolean).map((tag) => tag.toLowerCase());
 
   let tokenizer = null;
 
