8979a79fd13e1fd05ed4150e659a79353181af8b
Nikolay Kostyurin <jilizart@gmail.com>
2026-07-04 19:12:29 +0200

    chore: build tsconfig

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "eslint/config";
+import globals from "globals";
 import airbnb from "eslint-config-airbnb";
 import react from 'eslint-plugin-react'
 import jest from 'eslint-plugin-jest'
@@ -7,6 +8,12 @@ import _import from 'eslint-plugin-import'
 
 export default defineConfig([
   {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
     files: ["**/*.js"],
     plugins: {
       jest,
